3850d0de22f13ac56bfcba3cb794bd3b2207838b
yan_gao <ygao@techinsights.com>
2020-05-26 18:21:34 -0400

    remove credential

--- terraform.tf
+++ terraform.tf
@@ -5,8 +5,8 @@ terraform {
 provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
-  access_key = "AKIAVKHGDTDMEYBW5TW3"
-  secret_key = "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"
+  access_key = "***"
+  secret_key = "******"
 }
 
 module "lambda_archive" {
